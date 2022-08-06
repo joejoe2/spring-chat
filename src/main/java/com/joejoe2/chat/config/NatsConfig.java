a631fbbd33a4f1152828629be5264302e3af5aee
joejoe2 <39475537+joejoe2@users.noreply.github.com>
2022-08-06 11:51:11 +0800

    fix env

--- src/main/java/com/joejoe2/chat/config/NatsConfig.java
+++ src/main/java/com/joejoe2/chat/config/NatsConfig.java
@@ -3,6 +3,7 @@ package com.joejoe2.chat.config;
 import io.nats.client.Connection;
 import io.nats.client.Dispatcher;
 import io.nats.client.Nats;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,9 +11,12 @@ import java.io.IOException;
 
 @Configuration
 public class NatsConfig {
+    @Value("${nats.url}")
+    private String natsUrl;
+
     @Bean
     Connection natsConnection() throws IOException, InterruptedException {
-        return Nats.connect("nats://localhost:4222");
+        return Nats.connect(natsUrl);
     }
 
     @Bean
